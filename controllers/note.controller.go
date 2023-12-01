7e94c6c28bb1a13362c30499041389891b455968
Pavlo Karpovets <pavlok@defiyield.app>
2023-12-01 10:17:17 +0200

    vue app - go integration: JWT auth

--- controllers/note.controller.go
+++ controllers/note.controller.go
@@ -5,6 +5,7 @@ import (
 	"dazer/models"
 	"dazer/services"
 	"encoding/base64"
+	"encoding/hex"
 	"fmt"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/gin-gonic/gin"
@@ -141,6 +142,63 @@ func (nc *NoteController) ProtectedTest(ctx *gin.Context) {
 	})
 }
 
+func (nc *NoteController) ProtectedTestNew(ctx *gin.Context) {
+	bearerToken := ctx.Request.Header.Get("authorization")
+	if len(strings.Split(bearerToken, " ")) != 2 {
+		ctx.JSON(http.StatusUnauthorized, gin.H{"message": "authorization header is missing"})
+		return
+	}
+
+	token := strings.Split(bearerToken, " ")[1]
+	fmt.Println(token)
+	jwtParser := jwt.NewParser(jwt.WithoutClaimsValidation())
+
+	claims := &CustomClaims{}
+	parsed, parts, err := jwtParser.ParseUnverified(token, claims)
+	if err != nil {
+		ctx.JSON(http.StatusUnauthorized, gin.H{"message": err.Error()})
+		return
+	}
+
+	fmt.Println(parsed.Claims)
+	fmt.Println(parsed.Method)
+	fmt.Println(parsed.Header)
+	fmt.Println(parsed.Signature)
+	fmt.Println(parts)
+	fmt.Println(parts[2])
+
+	fmt.Println("signature", parts[2])
+	signature, err := hex.DecodeString(parts[2])
+	if err != nil {
+		ctx.JSON(http.StatusUnauthorized, gin.H{"message": err.Error()})
+		return
+	}
+	data, err := base64.RawURLEncoding.DecodeString(parts[1])
+	if err != nil {
+		ctx.JSON(http.StatusUnauthorized, gin.H{"message": err.Error()})
+		return
+	}
+	fmt.Println(string(data))
+	hash := crypto.Keccak256Hash(data)
+
+	publicKeyBytes, err := hex.DecodeString(claims.UserId)
+	if err != nil {
+		ctx.JSON(http.StatusUnauthorized, gin.H{"message": err.Error()})
+		return
+	}
+
+	signatureNoRecoverID := signature[:len(signature)-1] // remove recovery ID
+	verified := crypto.VerifySignature(publicKeyBytes, hash.Bytes(), signatureNoRecoverID)
+
+	parsed.Signature = []byte(parts[2])
+	ctx.JSON(http.StatusOK, gin.H{
+		"header":    parsed.Header,
+		"payload":   parsed.Claims,
+		"signature": parts[2],
+		"verified":  verified,
+	})
+}
+
 func (nc *NoteController) ProtectedTestMiddleware(ctx *gin.Context) {
 	claims, _ := ctx.Get("claims")
 	fmt.Println("ProtectedTestMiddleware", claims)
@@ -159,6 +217,7 @@ func (nc *NoteController) RegisterNoteRoutes(rg *gin.RouterGroup) {
 
 	protected := rg.Group("/protected")
 	protected.GET("/test", nc.ProtectedTest)
+	protected.GET("/test-new", nc.ProtectedTestNew) //supports signature verification from Vue app
 	protected.Use(middleware.JwtAuthMiddleware())
 	protected.GET("/test-middleware", nc.ProtectedTestMiddleware)
 }
